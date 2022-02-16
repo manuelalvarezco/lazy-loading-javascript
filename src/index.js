61271358de388ea26fa3ed1c84fb2b09946a292f
Luis Manuel Alvarez Bautista <lmalvarez@personalsoft.com.co>
2022-02-15 20:51:08 -0500

    finalizacion del proyecto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy";
 
-console.log('Happy hacking :)')
+// Javascript
+
+const minimun = 1;
+const maximun = 122;
+const randomImage = () => Math.floor(Math.random() * (maximun - minimun)) + minimun;
+
+
+const createNodeCard = () => {
+  // Card
+  const card = document.createElement('div'); 
+  card.className= 'card';
+
+  // Card-image
+  const cardImage = document.createElement('img');
+  cardImage.src = `https://randomfox.ca/images/${randomImage()}.jpg`;
+  cardImage.style = 'width: 100%';
+
+  card.appendChild(cardImage);
+
+  return card;
+}
+
+const mountNode = document.getElementById('images');
+mountNode.classList = "flex flex-col items-center";
+
+const addButton = document.querySelector('button');
+
+const addCard = () => {
+  const newCard = createNodeCard();
+  mountNode.append(newCard);
+  registerImage(newCard);
+}
+
+addButton.addEventListener('click', addCard);
+
